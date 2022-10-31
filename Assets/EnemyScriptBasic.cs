1d23df6b53e42a030c43f7fda9496e90750345d4
Alex Therwhanger <atherwha@trinity.edu>
2022-10-31 12:37:55 -0500

    trying alex branch

---
+++ Assets/EnemyScriptBasic.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScriptBasic : MonoBehaviour
+{
+    float health = 1.0f;
+    float speed = 1.5f;
+    bool dead = false;
+    int points = 0;
+    public GameObject enemy;
+    void Start()
+    {
+        //intitalize enemy
+    }
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collider){
+        if(collider.gameObject.tag == "bullet"){
+            isHit(enemy);
+        }
+
+    void isHit(GameObject obj){
+		health-=1;
+		isDead(obj);
+		}
+    }
+
+    void isDead(GameObject obj){
+        if(health <= 0){
+            dead = true;
+            points++;
+            Destroy(obj); //might need to destroy clone instead
+        }
+    }
+}
