cbdcbf4b2d47c6addae5acb1b5674c136cd7055d
Jacob Dean <74947691+jdean01@users.noreply.github.com>
2022-11-28 13:20:59 -0600

    enemy health used (#7)

--- Assets/Scripts/DummyEnemy.cs
+++ Assets/Scripts/DummyEnemy.cs
@@ -24,34 +24,8 @@ public class DummyEnemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distanceTraveled += speed * Time.deltaTime;
-        transform.position = start.PosOnPath(distanceTraveled);
+        //distanceTraveled += speed * Time.deltaTime;
+        //transform.position = start.PosOnPath(distanceTraveled);
     }
-    void OnTriggerEnter2D(Collider2D collider){
-        if(collider.gameObject.tag == "bullet"){
-            Projectile hitProjectile = collider.GetComponent<Projectile>();
-            if (hitProjectile != null) {
-                hitProjectile.HitEnemy();
-            }
-            isHit();
-        }
-
-        if(collider.gameObject.tag == "endpoint"){
-            Destroy(this.gameObject);
-            HealthUI.lives=HealthUI.lives-1;
-        }
-
-    void isHit(){
-		health-=1;
-		isDead(enemy);
-		}
-    }
-
-    void isDead(GameObject obj){
-        if(health <= 0){
-            dead = true;
-            points++;
-            Destroy(obj); //might need to destroy clone instead
-        }
-    }
+    
 }
